977e874c83d0991af9c5b9ce5889a4c0e8b531b1
dustn1259 <dustn1259@naver.com>
2020-09-10 00:42:13 +0900

    add python file[Test2.py]

---
+++ Test2.py
@@ -0,0 +1,12 @@
+import sys
+sys.stdin=open("input2.txt", "rt") #read file
+T=int(input())
+for i in range (T):
+    n, s, e, k=map(int, input().split())
+    a=list(map(int,input().split()))
+    a=a[s-1:e]
+    a.sort()
+    print("#%d %d" %(i+1,a[k-1]))
+    
+
+    
